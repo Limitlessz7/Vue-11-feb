6219e845758b15646a0fd6638e474b0bf8ad33e1
Limitlessz7 <Andijuliansyah27@gmail.com>
2026-02-11 11:20:58 +0700

    spasi

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <!-- tugas 1 -->
+
 <!-- <script setup lang="ts">
 import counterRef from './components/counterRef.vue';
 
@@ -10,55 +11,67 @@ import counterRef from './components/counterRef.vue';
 <!-- /tugas 1 -->
 
 <!-- tugas 2 -->
+
 <!-- <script setup lang="ts">
 import profileReactive from './components/profileReactive.vue';
 </script>
 <template>
   <profileReactive />
 </template> -->
+
 <!-- /tugas 2 -->
 
 <!-- tugas 3 -->
+
 <!-- <script setup lang="ts">
 import proxyDemo from './components/proxyDemo.vue';
 </script>
 <template>
   <proxyDemo />
 </template> -->
+
 <!-- /tugas 3 -->
 
 <!-- tugas 4 -->
+
 <!-- <script setup lang="ts">
 import destructureBug from './components/destructureBug.vue';
 </script>
 <template>
   <destructureBug />
 </template> -->
+
 <!-- /tugas 4 -->
 
 <!-- tugas 5 -->
+
 <!-- <script setup lang="ts">
 import nexttickDemo from './components/nexttickDemo.vue';
 </script>
 <template>
   <nexttickDemo />
 </template> -->
+
 <!-- /tugas 5 -->
 
 <!-- tugas 6 -->
+
 <!-- <script setup lang="ts">
 import unwrapCaveat from './components/unwrapCaveat.vue';
 </script>
 <template>
   <unwrapCaveat />
 </template>  -->
+
 <!-- /tugas 6 -->
 
 <!-- tugas 7 -->
+
 <!-- <script setup lang="ts">
 import shallowRefDemo from './components/shallowRefDemo.vue';
 </script>
 <template>
   <shallowRefDemo />
 </template>  -->
+
 <!-- /tugas 7 -->
